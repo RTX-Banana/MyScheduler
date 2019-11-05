339b215d84e2f916bdb977de3093e873a4ef724a
Davin Wong <54650468+xSpanky@users.noreply.github.com>
2019-11-05 01:12:56 -0800

    Update test_event.py
    Fixed Syntax Error

--- test_event.py
+++ test_event.py
@@ -6,7 +6,7 @@ def new_event():
     event = Event(event_name='CMPE 131', event_date='11/06/2019', event_timeStart='16:30:00', event_timeEnd='17:45:00')
     return event
     
-def test_new_event(new_event:
+def test_new_event(new_event):
     assert new_event.event_name == 'CMPE 131'
     assert new_event.event_date == '11/06/2019'
     assert new_event.event_timeStart == '16:30:00'
