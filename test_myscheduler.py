c62f8baac323820f7f11de45ac39488b05805681
xSpanky <davinlamwong@gmail.com>
2019-11-04 17:23:53 -0800

    Fix test

--- test_myscheduler.py
+++ test_myscheduler.py
@@ -4,10 +4,10 @@ from app.routes import create
 from app.routes import edit
 from app.routes import delete
 
-@pytest.fixture(scope='module')
-def new_create():
-    create = Create('CMPE131', '11/04/2019', '16:30:00', '17:45:00')
-    return create
+#@pytest.fixture(scope='module')
+#def new_create():
+#    create = Create('CMPE131', '11/04/2019', '16:30:00', '17:45:00')
+#    return create
 
 def test_new_create(new_create):
     assert new_create.event_name == 'CMPE131'
