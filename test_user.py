ddd974a7dd1311c9b20f0e54c70ebb1519a89e5c
Davin Wong <54650468+xSpanky@users.noreply.github.com>
2019-11-05 02:08:29 -0800

    Update test_user.py
    Trying random stuff

--- test_user.py
+++ test_user.py
@@ -11,3 +11,16 @@ def test_new_user(new_user):
     assert new_user.username == 'InnocentFear'
     assert new_user.email == 'InnocentFearBot@gmail.com'
     assert new_user.password_hash != 'TestUser123'
+    
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app('flasktest.cfg')
+    
+    testing_client = flask_app.test_client()
+    
+    app_ctx = flask_app.app_context()
+    app_ctx.push()
+    
+    yield test_client
+    
+    app_ctx.pop()
