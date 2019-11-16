ea66311a76acecf89885aa4e4bb16f6b74ca7b7e
Abhayakarmach <54560353+Abhayakarmach@users.noreply.github.com>
2019-11-16 12:21:06 -0800

    vacancy completed

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,8 @@ from flask_login import logout_user
 from flask_login import login_required
 from flask import request
 from werkzeug.urls import url_parse
+from array import array
+from datetime import datetime
 
 
 
@@ -43,9 +45,67 @@ def login():
 @login_required
 def userpage(username):
     user=User.query.filter_by(username=username).first_or_404()
-    e= Event.query.filter_by(user_id=user.id).all()
+    e= Event.query.filter(Event.user_id == user.id,  Event.event_name!='vacancy').all()
     
+    
+
     return render_template('userpage.html',user=user, title= 'Profile',e=e)
+
+@app.route('/vacancy/<username>/<fi>', methods=['GET', 'POST'])
+@login_required
+def vacancy(username, fi):
+    
+    user=User.query.filter_by(username=username).first_or_404()
+    event_to_vacancy=  Event.query.get_or_404(fi)
+
+    d= Event.query.filter(Event.user_id == user.id, Event.event_date==event_to_vacancy.event_date, Event.event_name=='vacancy').all()
+    for i in d:
+        db.session.delete(i)
+        db.session.commit()
+ 
+    e= Event.query.order_by('event_date', 'event_timeStart').filter(Event.user_id == user.id, Event.event_date=='2019-12-02 00:00:00.000000').all()
+    eventarray = []
+    for i in e:
+        eventarray.append(i)
+    
+    for j in range(len(eventarray)-1):
+       if (j==0 and eventarray[j].event_timeStart != '1900-01-01 00:00:00.000000'):
+        q = Event(event_name = 'vacancy', event_date = eventarray[j].event_date, event_timeStart=datetime(1900,1,1,00,00,00), event_timeEnd = eventarray[j].event_timeStart, user = current_user)
+        db.session.add(q)
+        db.session.commit() 
+        
+       c = Event(event_name = 'vacancy', event_date = eventarray[j].event_date, event_timeStart = eventarray[j].event_timeEnd, event_timeEnd = eventarray[j+1].event_timeStart, user = current_user)
+       db.session.add(c)
+       db.session.commit()  
+       
+    
+      
+    if (eventarray[-1].event_timeStart != datetime(1900, 1, 1, 23,59, 00)):
+        l = Event(event_name = 'vacancy', event_date = eventarray[1].event_date, event_timeStart = eventarray[-1].event_timeEnd, event_timeEnd = datetime(1900,1,1,23,59,00), user = current_user)
+        db.session.add(l)
+        db.session.commit()  
+
+    
+    k= Event.query.filter(Event.user_id == user.id,Event.event_name== 'vacancy', Event.event_date=='2019-12-02 00:00:00.000000').all()
+
+    return render_template('vacancy.html',user=user, title= 'Profile',k=k)
+    
+    
+    
+@app.route('/delete/<i>', methods=['GET', 'POST'])
+@login_required
+def delete(i):
+    Event_to_delete=  Event.query.get_or_404(i)
+    
+    try: 
+        db.session.delete(Event_to_delete)
+        db.session.commit()
+        return redirect(url_for('userpage',username=current_user.username))
+        
+    except:
+        return "there is a problem deleting n"
+
+    
     
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
@@ -65,18 +125,6 @@ def create():
     return render_template('create.html', title='Create', form=form)
     
 
-@app.route('/delete/<i>', methods=['GET', 'POST'])
-@login_required
-def delete(i):
-    Event_to_delete=  Event.query.get_or_404(i)
-    
-    try: 
-        db.session.delete(Event_to_delete)
-        db.session.commit()
-        return redirect(url_for('userpage',username=current_user.username))
-        
-    except:
-        return "there is a problem deleting n"
     
 @app.route('/edit/<e>', methods=['GET', 'POST'])
 @login_required
@@ -127,4 +175,6 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
     
+  
+
   
