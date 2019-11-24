036f18007ccec7421f7e2c3cb20fec0142682c11
Davin Wong <54650468+xSpanky@users.noreply.github.com>
2019-11-23 22:52:17 -0800

    Add files via upload
    create db for testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from config import Config
 from flask_login import LoginManager 
@@ -14,3 +16,25 @@ login=LoginManager(app)
 login.login_view='login'
 
 from app import routes, models
+
+db = SQLAlchemy()
+login=LoginManager()
+
+def create_app(test_config=None):
+    #Creating and configuring the app
+    app = Flask(__name__, instance_relative_config=False)
+    
+    if test_config is None:
+        #Load config instance
+        app.config.from_object('config.Config')
+    else:
+        app.config.from_mapping(test_config)
+    
+    db.init_app(app)
+    login.init_app(app)
+    login.login_view = 'login'
+    
+    with app.app_context():
+        from . import routes
+        db.create_all()
+        return app
