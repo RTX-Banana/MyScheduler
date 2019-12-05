8d84f8b203f7e2b9beeee4b0c6926b22478514e1
Davin Wong <54650468+Innocent-Fear@users.noreply.github.com>
2019-12-04 18:48:07 -0800

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,43 +1,22 @@
 import os
-
 from flask import Flask
 from config import Config
-from flask_login import LoginManager 
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_login import LoginManager
 
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-migrate=Migrate (app,db)
-login=LoginManager(app)
-login.login_view='login'
-
-from app import routes, models
-'''
 db = SQLAlchemy()
-login=LoginManager()
-#migrate=Migrate()
-#Look at migrate documentation for init_app
+login = LoginManager()
 
-def create_app(test_config=None):
-    #Creating and configuring the app
-    app = Flask(__name__, instance_relative_config=False)
-    
-    if test_config is None:
-        #Load config instance
-        app.config.from_object('config.Config')
-    else:
-        app.config.from_mapping(test_config)
-    
+def create_app(test_config=Config):
+    app = Flask(__name__)
+    app.debug = True
+    app.config.from_object(test_config)
     db.init_app(app)
     login.init_app(app)
+    login.init_app(app)
     login.login_view = 'login'
-    
+
     with app.app_context():
         from . import routes
         db.create_all()
         return app
-'''
